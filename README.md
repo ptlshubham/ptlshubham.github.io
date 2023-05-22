5237e43f349de5b0fb56e0e139755f7e3308fa84
Shubham Patel <ptlshubham@gmail.com>
2023-05-22 15:26:39 +0530

    logo

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# ptlshubham.github.io
-My Portfolio
+# Shubham Patel - Portfolio
+
+Welcome to my portfolio! I am Shubham Patel, a skilled web developer with expertise in Angular, HTML5, HTML, JavaScript, Node.js, CSS, GitHub, and Hostinger. This portfolio showcases my best work in web development, including Angular websites, mobile apps, websites, and portfolios. I am passionate about creating responsive and user-friendly web experiences, and I am always open to collaboration opportunities.
+
+## Technologies and Skills
+
+- Web Development
+- Angular Websites
+- Mobile App Development
+- Website Design
+- Portfolio Showcase
+- Angular
+- HTML5
+- HTML
+- JavaScript
+- Node.js
+- CSS
+- GitHub
+- Hostinger
+- Responsive Design
+- Front-end Development
+- UI/UX Design
+- Code Optimization
+- Cross-platform Development
+- Collaboration Opportunities
+
+## Projects
+
+Here are some notable projects I have worked on:
+
+1. **School Management System** - Developed a comprehensive web application for managing student records, attendance tracking, class scheduling, and exam management.
+
+2. **Saloon Management System** - Created a user-friendly system for saloon owners to manage appointments, employee schedules, and customer data, streamlining salon operations.
+
+3. **E-commerce Platform** - Built an end-to-end e-commerce website with features like product listings, shopping cart functionality, secure payment integration, and order management.
+
+4. **Scrap Metal Trading System** - Designed and developed a web application for buying and selling scrap metal, implementing user authentication, real-time pricing updates, and transaction management.
+
+Feel free to explore these projects to get an idea of my skills and capabilities as a web developer.
+
+## Contact
+
+I am open to collaboration opportunities and would love to discuss potential projects with you. If you have any questions or would like to get in touch, please feel free to reach out to me:
+
+- Email: [ptlshubham@gmail.com](mailto:ptlshubham@gmail.com)
+- LinkedIn: [https://www.linkedin.com/in/shubham-patel-bb5810b0/](https://www.linkedin.com/in/shubham-patel-bb5810b0/)
+- GitHub: [https://github.com/ptlshubham](https://github.com/ptlshubham)
+- Facebook:[https://www.facebook.com/shubham7696](https://www.facebook.com/shubham7696)
+- Instagram:[https://www.instagram.com/shubhampatel_oo7/](https://www.instagram.com/shubhampatel_oo7/)
+- Skype:[skype:ptlshubham@hotmail.com?chat](skype:ptlshubham@hotmail.com?chat)
+Let's work together to bring your ideas to life!
+
+## License
+
+The content of this portfolio is licensed under the [MIT License](https://opensource.org/licenses/MIT).
--- php/contact-form.php
+++ php/contact-form.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Name: 			Contact Form
-Written by: 	Okler Themes - (http://www.okler.net)
+Name:         	Contact Form
+Written by:     Okler Themes - (http://www.okler.net)
 Theme Version:	9.9.2
 */
 
@@ -25,64 +25,66 @@ $email = 'ptlshubham@gmail.com';
 // If the e-mail is not working, change the debug option to 2 | $debug = 2;
 $debug = 0;
 
-// If contact form don't has the subject input change the value of subject here
-$subject = ( isset($_POST['subject']) ) ? $_POST['subject'] : 'Define subject in php/contact-form.php line 29';
+// If contact form doesn't have the subject input, change the value of the subject here
+$subject = (isset($_POST['subject'])) ? $_POST['subject'] : 'Define subject in php/contact-form.php line 29';
 
 $message = '';
 
-foreach($_POST as $label => $value) {
-	$label = ucwords($label);
+foreach ($_POST as $label => $value) {
+    $label = ucwords($label);
 
-	// Use the commented code below to change label texts. On this example will change "Email" to "Email Address"
+    // Use the commented code below to change label texts. On this example will change "Email" to "Email Address"
 
-	// if( $label == 'Email' ) {               
-	// 	$label = 'Email Address';              
-	// }
+    // if ($label == 'Email') {               
+    //     $label = 'Email Address';              
+    // }
 
-	// Checkboxes
-	if( is_array($value) ) {
-		// Store new value
-		$value = implode(', ', $value);
-	}
+    // Checkboxes
+    if (is_array($value)) {
+        // Store new value
+        $value = implode(', ', $value);
+    }
 
-	$message .= $label.": " . nl2br(htmlspecialchars($value, ENT_QUOTES)) . "<br>";
+    $message .= $label . ": " . nl2br(htmlspecialchars($value, ENT_QUOTES)) . "<br>";
 }
 
 $mail = new PHPMailer(true);
 
 try {
 
-	$mail->SMTPDebug = $debug;                                 // Debug Mode
+    $mail->SMTPDebug = $debug;                                 // Debug Mode
 
-	// Step 2 (Optional) - If you don't receive the email, try to configure the parameters below:
+    // Step 2 (Optional) - If you don't receive the email, try to configure the parameters below:
 
-	//$mail->IsSMTP();                                         // Set mailer to use SMTP
-	//$mail->Host = 'mail.yourserver.com';				       // Specify main and backup server
-	//$mail->SMTPAuth = true;                                  // Enable SMTP authentication
-	//$mail->Username = 'user@example.com';                    // SMTP username
-	//$mail->Password = 'secret';                              // SMTP password
-	//$mail->SMTPSecure = 'tls';                               // Enable encryption, 'ssl' also accepted
-	//$mail->Port = 587;   								       // TCP port to connect to
+    $mail->isSMTP();                                         // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';                           // Specify main and backup server
+    $mail->SMTPAuth = true;                                  // Enable SMTP authentication
+    $mail->Username = 'ptlshubham@gmail.com';                 // SMTP username (your Gmail address)
+    $mail->Password = 'your-password';                        // SMTP password (your Gmail password)
+    $mail->SMTPSecure = 'tls';                               // Enable encryption, 'ssl' also accepted
+    $mail->Port = 587;                                       // TCP port to connect to
 
-	$mail->AddAddress($email);	 						       // Add another recipient
+    $mail->addAddress($email);                               // Add another recipient
 
-	//$mail->AddAddress('person2@domain.com', 'Person 2');     // Add a secondary recipient
-	//$mail->AddCC('person3@domain.com', 'Person 3');          // Add a "Cc" address. 
-	//$mail->AddBCC('person4@domain.com', 'Person 4');         // Add a "Bcc" address. 
+    //$mail->AddAddress('person2@domain.com', 'Person 2');     // Add a secondary recipient
+    //$mail->AddCC('person3@domain.com', 'Person 3');          // Add a "Cc" address. 
+    //$mail->AddBCC('person4@domain.com', 'Person 4');         // Add a "Bcc" address. 
 
-	// From - Name
-	$fromName = ( isset($_POST['name']) ) ? $_POST['name'] : 'Website User';
-	$mail->SetFrom($email, $fromName);
+    // From - Name
+    $fromName = (isset($_POST['name'])) ? $_POST['name'] : 'Website User';
+    $mail->SetFrom($email, $fromName);
 
-	// Reply To
-	if( isset($_POST['email']) && !empty($_POST['email']) ) {
-		$mail->AddReplyTo($_POST['email'], $fromName);
-	}
+    // Reply To
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $mail->AddReplyTo($_POST['email'], $fromName);
+    }
 
-	$mail->IsHTML(true);                                       // Set email format to HTML
+    $mail->IsHTML(true);                                       // Set email format to HTML
 
-	$mail->CharSet = 'UTF-8';
+    $mail->CharSet = 'UTF-8';
 
+    $mail
+	
 	$mail->Subject = $subject;
 	$mail->Body    = $message;
 
